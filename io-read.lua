4275ebb04a2f21cf42eb1b352c822d490f63cbc2
Harry <jarriztg@users.noreply.github.com>
2016-09-23 03:32:30 -0500

    creating script

---
+++ io-read.lua
@@ -0,0 +1,15 @@
+print("Hey! Whats your name?")
+name = io.read()
+print("How old are you?")
+age = io.read()
+print("Whats your country?")
+country = io.read()
+print("OK")
+print("Name:", name, "\nAge:", age, "\nCountry:", country)
+
+--[[
+OK
+Name:   Tommy
+Age:    18
+Country:        Mexico
+]]
